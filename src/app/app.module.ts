bcca75573935b6163b178d7d1ee523e05ebee9ac
Mukesh Singh <mukesh.singh@hitachiconsulting.com>
2018-04-08 10:14:34 +0530

    created teams

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
+import { TeamsComponent } from './teams/teams.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    TeamsComponent
   ],
   imports: [
     BrowserModule
